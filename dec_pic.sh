5544c02c318ac5cb6ab027704756fc7628f2a772
Potapov TMK <potapov@tmkin.ru>
2017-04-13 20:01:16 +0300

    отладкака getopts u:l:o:h - функций скачивания и пр. + .gitignore

--- dec_pic.sh
+++ dec_pic.sh
@@ -1,38 +1,112 @@
 #!/bin/bash
+#Выбор из цифрового (децимального) списка строк по текстовым и фото подсказкам
+#на примере Классификатора ЕСКД. 
 
-#скачивание
-#wget -r -k -l 5 -nc -np http://classinform.ru/ok-eskd/kod.html
-#exit 0
+#__скачивание__ - download / сбор материалов по URL
+#__подготовка__ - from download to project / формирование списка строк из заголовков HTML <title>
+#__работа__     - work for string & img / выбор по текстовым подсказкам (спискам вариантов) с выводом изображений (при наличии)
 
-#подготовка
+
+#входные аргументы
+while getopts "u:l:o:h" opt;do
+    case $opt in
+        u) url1="$OPTARG"
+        ;;
+        l) ptch="$OPTARG"
+        ;;
+        o) pout="$OPTARG"
+        ;;
+        h) help=1
+        ;;
+    esac
+done
+pold="$PWD"
+echo "url1=$url1"
+echo "ptch=$ptch"
+echo "pout=$pout"
+echo "pold=$pold"
+if [ -n "$help" ];then echo "Использование: $0 - только запуск готового списка в текущем каталоге.
+
+Варианты использования с ключами:
+-u       URL / ссылка для скачивания указанного сайта в текущий каталог
+-l       patch of local copy / каталог с локальной копией из которой создаётся список строк;
+-o       patch of work list string / использовать для запуска данный каталог списка строк;
+
+$0 -u \"http://example.com/one/three.html\" -l \"/patch/to/local\" -o \"/patch/git/proj\"
+-u & -l & -o       скачать из \"-u\" в \"-l\", сформировать список строк в \"-o\" и запустить;
+
+-u & -l        скачать в \"-l\" и сформировать список строк в нём-же;
+-l & -o        из \"-l\" сформировать список строк в \"-o\";
+-u & -o        скачать в \"-l\" без формирования списка.
+"
+fi
+
+
+#__скачивание__
+#Первая смена локального каталога
+if [ -d "$ptch" ];then 
+        cd "$ptch"
+    elif [ -d "$pout" ];then 
+        cd "$pout"
+fi
+
+if [ -n "$url1" ];then 
+    echo "Скачивание из $url1 в локальный каталог $PWD"
+    #требуются файлы с именами из цифр, например 123456.html и img/7123.jpg
+    #url1="http://classinform.ru/ok-eskd/kod.html"
+    #
+    #wget -r -k -l 5 -nc -np http://classinform.ru/ok-eskd/kod.html
+fi
+
+
+#__подготовка__
+#каталог загрузки
+#ptch="/home/$USER/tst/zayavki/ok-eskd/classinform.ru/ok-eskd"
 #рабочий каталог
-ptch="/home/$USER/tst/zayavki/ok-eskd/classinform.ru/ok-eskd"
-ptch="/home/$USER/git/dec_pic"
-#имя ASCII файла со строками 
-outf="eskd_num.txt"
-#имя сжатого gzip файла со строками 
-outgz="$outf.gz"
-#подкаталог с изображениями
-outpicdir="." #если в том-же подкаталоге
-outpicdir="img" #всё это только для файлов состоящих из цифр, например 7111.jpg
+if [ -d "$ptch" ];then
+    #Вторая смена локального каталога
+    if [ -d "$pout" ];then
+        cd "$pout"
+    fi
+    echo "Из заголовков HTML каталога $ptch формируется список строк в локальный каталог $PWD"
+    #cd "$pout"
+    #else
+    #pout="$PWD"
 
-#cat $ptch/kod-0*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>$outf
-#cat $ptch/kod-1*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#cat $ptch/kod-2*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#cat $ptch/kod-3*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#cat $ptch/kod-4*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#cat $ptch/kod-5*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#cat $ptch/kod-6*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#cat $ptch/kod-7*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#cat $ptch/kod-8*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#cat $ptch/kod-9*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
-#сжатие ОДНОГО файла BD
-#gzip -c $outf > $outgz
-#сжатие КАТАЛОГА с изображениями
-#tar -cvzf $outpicgz $outpicdir/
-#exit 0
+    #pout="/home/$USER/git/dec_pic"
+    #имя формируемого ASCII файла - каталога строк 
+    outf="eskd_num.txt"
+    #имя сжатого gzip файла со строками 
+    #outgz="$pout/$outf.gz"
+    outgz="$outf.gz"
+    #подкаталог с изображениями
+    outpicdir="." #если в том-же подкаталоге
+    outpicdir="img" #всё это только для файлов состоящих из цифр, например 7111.jpg
 
-#работа
+    #cat $ptch/kod-0*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>$outf
+    #cat $ptch/kod-1*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #cat $ptch/kod-2*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #cat $ptch/kod-3*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #cat $ptch/kod-4*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #cat $ptch/kod-5*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #cat $ptch/kod-6*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #cat $ptch/kod-7*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #cat $ptch/kod-8*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #cat $ptch/kod-9*.html|sed -n -e '/<title/p'|iconv -f WINDOWS-1251 -t UTF-8|sed 's/<title>Классификатор\ ЕСКД\ |\ //'|sed 's/\ |\ Общероссийский\ классификатор\ изделий\ и\ конструкторских\ документов<\/title>//'>>$outf
+    #сжатие ОДНОГО файла BD
+    #gzip -c $outf > $outgz
+    #сжатие КАТАЛОГА с изображениями
+    #tar -cvzf $outpicgz $outpicdir/
+fi
+pwd
+cd "$pold"
+pwd
+exit 0
+
+
+
+#__работа__
+#_настройка_
 #Имя временного файла совмещённого изображения
 pict=t.jpg
 #Имя файла HTML #<html><body><img src="t.jpg"><p></body>
@@ -51,14 +125,17 @@ slv3=Код
 N=2
 #Количество цифр в списке всего
 M=6
-#Варианты использования GNU утилит
+
+#_Варианты использования GNU утилит_
 #montage img/711[0-9].jpg -tile 2x -geometry '300x200+2+2>' t.jpg
 #$browser /home/$USER/git/eskd_num/t.htm
 #montage img/731[0-9].jpg -tile 2x -geometry '300x200+2+2>' t.jpg;$browser /home/$USER/git/eskd_num/t.htm
 #read -n 1 a;echo $a;montage img/7"$a"1[0-9].jpg -tile 2x -geometry '300x200+2+2>' t.jpg;$browser /home/$USER/git/eskd_num/t.htm
 #if `ls img/7111[0-9].* > /dev/null 2>&1`;then echo yyyyy;else echo nnnnn;fi
 
-#Вывод начального списка
+#переход в каталог
+if [ -d "$pout" ];then cd "$pout";fi
+#_Вывод начального списка_
 zcat $outgz|sed -n -e "/^$slv2/p"
 echo ""
 #ввод N цифр из начального списка (в примере 2 цифры)
@@ -66,7 +143,7 @@ read -p "Введите $N цифры $slv2а:" -n $N F
 #перебор оставшихся цифр начинается со следующей после N
 N=$(($N+1))
 
-#перебор оставшихся цифр
+#_перебор оставшихся цифр_
 for N in `seq $N $M` ;do
     echo ""
     echo ""
@@ -89,3 +166,5 @@ echo ""
 echo ""
 #выборка и вывод подходящих строк, показать все поля
 zcat $outgz|sed -n -e "/^$slv3\ $F"\ /p
+#возвращение в исходный каталог
+cd "$pold"
